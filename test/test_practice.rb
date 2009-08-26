4884fac3d9f8dcc5cccbe78ac1ec78763335a889
sora_harakami <sora134@gmail.com>
2009-08-26 22:23:48 +0900

    writed test/test_practice.rb

--- test/test_practice.rb
+++ test/test_practice.rb
@@ -1,1 +1,20 @@
+def convert_ary_to_hash(ary)
+    if ary.length % 2 == 0
+        return Hash[*ary]
+    else
+        raise ArgumentError, "argment error: not even-numbered ary.length"
+    end
+end
 
+def check_exception
+    begin
+        convert_ary_to_hash([1,2,3,4,5])
+    rescue ArgumentError
+        true
+    end
+end
+def check_type
+    convert_ary_to_hash([1,2,3,4]).kind_of?(hash)
+end
+p check_exception
+p check_type
