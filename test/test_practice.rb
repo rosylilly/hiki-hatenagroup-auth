fbcbede4f03f420cd0af02d3f62d04409835fff4
sora_harakami <sora134@gmail.com>
2009-08-26 22:29:23 +0900

    fixed test/test_practice.rb

--- test/test_practice.rb
+++ test/test_practice.rb
@@ -1,20 +1,19 @@
-def convert_ary_to_hash(ary)
-    if ary.length % 2 == 0
-        return Hash[*ary]
-    else
-        raise ArgumentError, "argment error: not even-numbered ary.length"
-    end
-end
-
-def check_exception
+def test_convert_ary_to_hash
     begin
-        convert_ary_to_hash([1,2,3,4,5])
+        ary = [1,2,3,4,5]
+        if ary.length % 2 == 0
+            hash = Hash[*ary]
+        else
+            raise ArgumentError, "argument error: not even-numbered ary.length"
+        end
     rescue ArgumentError
-        true
+        ary = [1,2,3,4]
+        if ary.length % 2 == 0
+            hash = Hash[*ary]
+        else
+            raise ArgumentError, "argument error: not even-numbered ary.length"
+        end
+        return true if hash.kind_of?(Hash)
     end
 end
-def check_type
-    convert_ary_to_hash([1,2,3,4]).kind_of?(Hash)
-end
-p check_exception
-p check_type
+p test_convert_ary_to_hash 
