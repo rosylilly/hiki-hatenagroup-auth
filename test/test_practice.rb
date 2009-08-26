1f33f8f74d7351ece83a880c34afdf0220a7b079
sora_harakami <sora134@gmail.com>
2009-08-27 07:06:10 +0900

    fixed test/test_practice.rb

--- test/test_practice.rb
+++ test/test_practice.rb
@@ -1,11 +1,3 @@
-def test_convert_ary_to_hash
-    begin
-        convert_ary_to_hash([1,2,3,4,5])
-    rescue ArgumentError
-        return true if convert_ary_to_hash([1,2,3,4]).kind_of?(Hash)
-    end
-end
-
 class TestConvertAryToHash < Test::Unit::TestCase
     def test_exception
         assert_raise(ArgumentError) do
