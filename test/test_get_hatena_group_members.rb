ebd782a39d820f59a9e8a0e3d3ca373334d37cd8
sora_harakami <sora134@gmail.com>
2009-08-27 14:15:35 +0900

    fixed test/test_get_hatena_group_members.rb / added lib/hatena_group.rb

--- test/test_get_hatena_group_members.rb
+++ test/test_get_hatena_group_members.rb
@@ -1,14 +1,18 @@
+require "lib/hatena_group.rb"
 class TestGetHatenaGroupMembers < Test::Unit::TestCase
-    def test_argument
+    def test_get_hatena_group_members
         assert_raise(ArgumentError) do
             get_hatena_group_members([])
         end
+        
+        assert_raise(ArgumentError) do
+            get_hatena_group_members("sonzaisinayoaaaaajfiwea")
+        end
+
         assert_nothing_raised() do
             get_hatena_group_members('generation1991')
         end
-    end
 
-    def test_type
         assert_kind_of(Array,get_hatena_group_members('generation1991'))
     end
 end
